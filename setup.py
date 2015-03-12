158161c55d00d898a3f7ba39e2b3391e5003d43c
Alexey Popravka <alexey.popravka@horsedevel.com>
2015-03-12 08:13:41 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+import re
+
+from setuptools import find_packages, setup
+
+
+def read_version():
+    regexp = re.compile(r"^__version__\W*=\W*'([\d.abrc]+)'")
+    init_py = os.path.join(os.path.dirname(__file__), 'argsrun', '__init__.py')
+    with open(init_py) as f:
+        for line in f:
+            match = regexp.match(line)
+            if match is not None:
+                return match.group(1)
+        else:
+            raise RuntimeError('Cannot find version in argsrun/__init__.py')
+
+
+install_requires = []
+
+setup(name="ArgsRun",
+      version=read_version(),
+      description="Simple library for creating commands & subcommands",
+      platforms=["POSIX"],   # XXX
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=install_requires,
+      entry_points={
+        'console_scripts': [
+            'argsrun = argsrun.main:main',
+            ],
+        'argsrun': [
+            'version = argsrun.main:version',
+            ],
+        },
+      )
